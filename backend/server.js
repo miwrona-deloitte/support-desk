32644b3be1a5e75266bdc592f7634b05a7396a45
Michal Wrona <miwrona@deloittece.com>
2022-07-06 14:13:29 +0200

    user controller

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,10 @@ const PORT = process.env.PORT || 5000
 const app = express()
 
 app.get('/', (req,res) => {
-    // res.send('Hello')
-    // res.json({message: 'Welcome to the Support Desk API'})
     res.status(200).json({message: 'Welcome to the Support Desk API'})
 })
+
+// Routes 
+app.use('/api/users', require('./routes/userRoutes'))
 
 app.listen(PORT, () => {console.log(`Server started on port ${PORT}`)})
