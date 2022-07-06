7d6020516a5575b5bbbeec1575b5fe32c3d6d4d7
Michal Wrona <miwrona@deloittece.com>
2022-07-06 15:41:15 +0200

    error handling; async

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,25 @@
-const registerUser = (req, res) => {
-    res.send('Register Route')
-}
+const expressAsyncHandler = require('express-async-handler')
 
-const loginUser = (req, res) => {
+// @desc Register a new user
+// @route /api/users 
+// @access Public
+const registerUser = expressAsyncHandler(async (req, res) => {
+    const {name, password, email} = req.body
+
+    // Validation
+    if (!name || !password || !email) {
+        res.status(400)
+        throw new Error('Please include name, password and email in body params')
+    }
+    res.send('Register Route')
+}) 
+
+// @desc Login a user
+// @route /api/users/login 
+// @access Public
+const loginUser = expressAsyncHandler(async (req, res) => {
     res.send('Login Route')
-}
+})
 
 module.exports = {
     loginUser, 
