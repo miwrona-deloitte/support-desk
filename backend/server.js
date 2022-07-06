c95c0865e046872595ed66e3effa7b619b3da555
Michal Wrona <miwrona@deloittece.com>
2022-07-06 16:01:09 +0200

    db connection

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 const express = require('express')
+const colors = require('colors')
 const dotenv = require('dotenv').config()
 const { errorHandler } = require('./middleware/errorMiddleware')
 const PORT = process.env.PORT || 5000
+const connectDB = require('./config/db')
 
 const app = express()
+
+// Connect to database
+connectDB()
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
