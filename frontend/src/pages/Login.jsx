8ed6cc19b322c6fb7caf9d72bacafbe1a33e2654
Michal Wrona <miwrona@deloittece.com>
2022-07-10 10:13:10 +0200

    form data -> redux; login/register

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { toast } from 'react-toastify';
 import { FaSignInAlt } from 'react-icons/fa';
+import { useSelector, useDispatch } from 'react-redux';
+import { login } from '../features/auth/authSlice';
 
 function Login() {
     const [formData, setFormData] = useState({
@@ -10,6 +12,10 @@ function Login() {
 
     const { email, password } = formData;
 
+    const dispatch = useDispatch();
+
+    const { user, isSuccess, isLoading, message } = useSelector(state => state.auth);
+
     const onChange = e => {
         setFormData(prevState => ({
             ...prevState,
@@ -18,6 +24,13 @@ function Login() {
     };
     const onSubmit = e => {
         e.preventDefault();
+
+        const userData = {
+            email,
+            password,
+        };
+
+        dispatch(login(userData));
     };
 
     return (
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { toast } from 'react-toastify';
 import { FaUser } from 'react-icons/fa';
+import { useSelector, useDispatch } from 'react-redux';
+import { register } from '../features/auth/authSlice';
 
 function Register() {
     const [formData, setFormData] = useState({
@@ -11,6 +13,10 @@ function Register() {
     });
 
     const { name, email, password, password2 } = formData;
+
+    const dispatch = useDispatch();
+
+    const { user, isSuccess, isLoading, message } = useSelector(state => state.auth);
 
     const onChange = e => {
         setFormData(prevState => ({
@@ -23,6 +29,13 @@ function Register() {
 
         if (password !== password2) {
             toast.error('Password do not match');
+        } else {
+            const userData = {
+                name,
+                email,
+                password,
+            };
+            dispatch(register(userData));
         }
     };
 
@@ -31,7 +44,7 @@ function Register() {
             <section className='heading'>
                 <h1>
                     <FaUser />
-                    Register
+                    Register {user}
                 </h1>
                 <p>Please create an account</p>
             </section>
